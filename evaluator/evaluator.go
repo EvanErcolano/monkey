02f7f8796e7b224d8a4b6584740ec93f621fd2a5
Evan Ercolano <evanercolano@gmail.com>
2019-07-04 18:58:00 -0400

    Add support for eight infix operators for integers

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -16,15 +16,19 @@ func Eval(node ast.Node) object.Object {
 
 	case *ast.Program:
 		return evalStatements(node.Statements)
-	case *ast.PrefixExpression:
-		right := Eval(node.Right)
-		return evalPrefixExpession(node.Operator, right)
-	case *ast.ExpressionStatement:
-		return Eval(node.Expression)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpession(node.Operator, right)
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+		return evalInfixEpression(node.Operator, left, right)
 	}
 
 	return nil
@@ -78,4 +82,48 @@ func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
 
 	value := right.(*object.Integer).Value
 	return &object.Integer{Value: -value}
+}
+
+func evalInfixEpression(
+	operator string,
+	left, right object.Object,
+) object.Object {
+
+	switch {
+	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
+		return evalIntegerInfixExpression(operator, left, right)
+	default:
+		return NULL
+	}
+}
+
+func evalIntegerInfixExpression(
+	operator string,
+	left, right object.Object,
+) object.Object {
+	leftVal := left.(*object.Integer).Value
+	rightVal := right.(*object.Integer).Value
+
+	switch operator {
+	case "+":
+		return &object.Integer{Value: leftVal + rightVal}
+	case "-":
+		return &object.Integer{Value: leftVal - rightVal}
+	case "*":
+		return &object.Integer{Value: leftVal * rightVal}
+	case "/":
+		return &object.Integer{Value: leftVal / rightVal}
+	case "<":
+		return nativeBoolToBooleanObject(leftVal < rightVal)
+	case ">":
+		return nativeBoolToBooleanObject(leftVal > rightVal)
+	case "==":
+		return nativeBoolToBooleanObject(leftVal == rightVal)
+	case "!=":
+		return nativeBoolToBooleanObject(leftVal != rightVal)
+	// case "%": TODO IMPL modulo here and in parser
+	// 	return &object.Integer{Value: leftVal % rightVal}
+	default:
+		return NULL
+	}
 }
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -16,7 +16,7 @@ func TestBangOperator(t *testing.T) {
 		{"!false", true},
 		{"!5", false}, // we're deciding 5 is "truthy"
 		{"!!true", true},
-		{"!!false", true},
+		{"!!false", false},
 		{"!!5", true},
 	}
 
@@ -32,7 +32,15 @@ func TestEvalBooleanExpression(t *testing.T) {
 		expected bool
 	}{
 		{"true", true},
-		{"false", false},
+        {"false", false},
+        {"1 < 2", true},
+        {"1 > 2", false},
+        {"1 < 1", false},
+        {"1 > 1", false},
+        {"1 == 1", true},
+        {"1 != 1", false},
+        {"1 == 2", false},
+		{"1 != 2", true},
 	}
 
 	for _, tt := range tests {
@@ -60,10 +68,21 @@ func TestEvalIntegerExpression(t *testing.T) {
 		input    string
 		expected int64
 	}{
-		{"5", 5},
-		{"10", 10},
-		{"-5", -5},
-		{"-10", -10},
+        {"5", 5},
+        {"10", 10},
+        {"-5", -5},
+        {"-10", -10},
+        {"5 + 5 + 5 + 5 - 10", 10},
+        {"2 * 2 * 2 * 2 * 2", 32},
+        {"-50 + 100 + -50", 0},
+        {"5 * 2 + 10", 20},
+        {"5 + 2 * 10", 25},
+        {"20 + 2 * -10", 0},
+        {"50 / 2 * 2 + 10", 60},
+        {"2 * (5 + 10)", 30},
+        {"3 * 3 * 3 + 10", 37},
+        {"3 * (3 * 3) + 10", 37},
+        {"(5 + 10 * 2 + 15 / 3) * 2 + -10", 50},
 	}
 
 	for _, tt := range tests {
