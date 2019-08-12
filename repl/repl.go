5bd5ccdd50aaaeaa19233245ff39b0035efbe53a
Evan Ercolano <evanercolano@gmail.com>
2019-08-12 09:21:10 -0400

    Extending our REPL to use the macro expansion phase

--- repl/repl.go
+++ repl/repl.go
@@ -28,6 +28,7 @@ const PROMPT = ">> "
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 	env := object.NewEnvironment()
+	macroEnv := object.NewEnvironment()
 
 	for {
 		// TODO: in order to allow multi line input (common for closures) we will need
@@ -51,7 +52,10 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program, env)
+		evaluator.DefineMacros(program, macroEnv)
+		expanded := evaluator.ExpandMacros(program, macroEnv)
+
+		evaluated := evaluator.Eval(expanded, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
