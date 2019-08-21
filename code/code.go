7452b4fa36286adf34e029a00cb1bb2abe53852d
Evan Ercolano <evanercolano@gmail.com>
2019-08-21 16:21:09 -0400

    Add ability to Disassemble our bytecode

--- code/code.go
+++ code/code.go
@@ -1,13 +1,52 @@
 package code
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
 )
 
 type Instructions []byte
 
-type Opcode byte // aliasing bytes to type opcode
+// String outputs our byte slice instructions in a more readable form
+func (ins Instructions) String() string {
+	var out bytes.Buffer
+
+	i := 0
+	for i < len(ins) {
+		def, err := Lookup(ins[i])
+		if err != nil {
+			fmt.Fprintf(&out, "ERROR: %s\n", err)
+			continue
+		}
+
+		operands, read := ReadOperands(def, ins[i+1:])
+
+		fmt.Fprintf(&out, "%04d %s\n", i, ins.fmtInstruction(def, operands))
+
+		i += 1 + read
+	}
+
+	return out.String()
+}
+
+func (ins Instructions) fmtInstruction(def *Definition, operands []int) string {
+	operandCount := len(def.OperandWidths)
+
+	if len(operands) != operandCount {
+		return fmt.Sprintf("ERROR: operand len %d does not match defined %d\n",
+			len(operands), operandCount)
+	}
+
+	switch operandCount {
+	case 1:
+		return fmt.Sprintf("%s %d", def.Name, operands[0])
+	}
+
+	return fmt.Sprintf("ERROR: unhandled operandCount for %s\n", def.Name)
+}
+
+type Opcode byte
 
 const (
 	OpConstant Opcode = iota
@@ -23,9 +62,11 @@ type Definition struct {
 // Lookup for how many operands an opcode has and
 // what its human-readable name is
 var definitions = map[Opcode]*Definition{
-	OpConstant: {"OpConstant", []int{2}}, // only operand is 2 bytes wide
+	OpConstant: &Definition{"OpConstant", []int{2}}, // only operand is 2 bytes wide
 }
 
+// Lookup looks up an Opcode definition via our definition map
+// Either returns a Definition or an opcode undefined error
 func Lookup(op byte) (*Definition, error) {
 	def, ok := definitions[Opcode(op)]
 	if !ok {
@@ -34,20 +75,30 @@ func Lookup(op byte) (*Definition, error) {
 	return def, nil
 }
 
+// Make takes an opcode byte and int operands and converts
+// them into a slice of bytes repr'ing our bytecode
+// Assembling from human readable instructions to our VM bytecode
+// Ex. Opconstant will contain:
+// [bytecode instruction, 1/2 constant byte, 2/2 constant byte]
+// If we add the constant 1 our returned bytecode will be:
+// [0, 00000000, 00000001]
 func Make(op Opcode, operands ...int) []byte {
 	def, ok := definitions[op]
 	if !ok {
 		return []byte{}
 	}
 
-	instructionLen := 1 // for the initial opcode
+	instructionLen := 1 // index 0 contains our Opcode bytecode
 	for _, w := range def.OperandWidths {
 		instructionLen += w
 	}
 
+	// create a byte slice big enough for our instruction length
 	instruction := make([]byte, instructionLen)
+	// place our Opcode bytcode
 	instruction[0] = byte(op)
 
+	// append our operands as bytecode onto the byte slice
 	offset := 1
 	for i, o := range operands {
 		width := def.OperandWidths[i]
@@ -58,4 +109,28 @@ func Make(op Opcode, operands ...int) []byte {
 		offset += width
 	}
 	return instruction
+}
+
+// ReadOperands takes an instruction definition and some VM bytecode
+// and uses that definition to convert the bytecode back to its
+// integer representation. Disassembling back to human readable form.
+func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
+	operands := make([]int, len(def.OperandWidths))
+	offset := 0
+
+	for i, width := range def.OperandWidths {
+		switch width {
+		case 2:
+			operands[i] = int(ReadUint16(ins[offset:]))
+		}
+
+		offset += width
+	}
+
+	return operands, offset
+}
+
+// ReadUint16 reads a byteslice and converts it to an integer.
+func ReadUint16(ins Instructions) uint16 {
+	return binary.BigEndian.Uint16(ins)
 }
