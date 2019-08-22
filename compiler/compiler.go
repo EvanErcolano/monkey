6c3792dab7d2d58282a3ab523b73dbdc0c6c55bd
Evan Ercolano <evanercolano@gmail.com>
2019-08-22 10:49:51 -0400

    Compile Integer literals down to our bytecode format

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -21,7 +21,59 @@ func New() *Compiler {
 
 // Compile our ast down to it's respective bytecode representation.
 func (c *Compiler) Compile(node ast.Node) error {
+	switch node := node.(type) {
+	case *ast.Program:
+		for _, s := range node.Statements {
+			err := c.Compile(s)
+			if err != nil {
+				return nil
+			}
+		}
+	case *ast.ExpressionStatement:
+		err := c.Compile(node.Expression)
+		if err != nil {
+			return err
+		}
+	case *ast.InfixExpression:
+		err := c.Compile(node.Left)
+		if err != nil {
+			return err
+		}
+
+		err = c.Compile(node.Right)
+		if err != nil {
+			return err
+		}
+	case *ast.IntegerLiteral:
+		// Convert the AST Integer Literal to our internal object
+		// representation and then add it into our constant pool.
+		// Uses the constant pool index to that literal to generate a
+		// bytecode constant instruction with reference to the index.
+		integer := &object.Integer{Value: node.Value}
+		c.emit(code.OpConstant, c.addConstant(integer))
+
+	}
 	return nil
+}
+
+// addConstant appends an object constant to our constant pool
+// it returns the index to our the added constant.
+func (c *Compiler) addConstant(obj object.Object) int {
+	c.constants = append(c.constants, obj)
+	return len(c.constants) - 1
+}
+
+// emit generates bytecode instructions and add them into memory.
+func (c * Compiler) emit(op code.Opcode, operands ...int) int {
+	ins := code.Make(op, operands...)
+	pos := c.addInstruction(ins)
+	return pos
+}
+
+func (c* Compiler) addInstruction(ins []byte) int {
+	posNewInstruction := len(c.instructions)
+	c.instructions = append(c.instructions, ins...)
+	return posNewInstruction
 }
 
 func (c *Compiler) ByteCode() *Bytecode {
